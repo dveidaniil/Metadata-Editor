788b9ec540cbce580040e32959bcb8ab5f03f858
dveidaniil <127894432+dveidaniil@users.noreply.github.com>
2025-06-02 23:07:54 +0300

    ебать мой хуй... оно работает

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml;
+using System.Security.Cryptography;
 
 
 
@@ -49,12 +52,10 @@ namespace Metadata_Editor
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                string filename = Path.GetFileName(openFileDialog1.FileName);
-                string filePath = openFileDialog1.FileName;
-
-                
-
-
+                filename = Path.GetFileName(openFileDialog1.FileName);
+                filepath = openFileDialog1.FileName;
+                // например, вывести путь на форму, если хотите
+                // label1.Text = filename;
             }
         }
 
@@ -76,7 +77,7 @@ namespace Metadata_Editor
 
         private void button4_Click(object sender, EventArgs e)
         {
-            
+
             textBox1.Text = userName;
             textBox1.ForeColor = Color.Black;
             textBox3.Text = userName;
@@ -85,7 +86,49 @@ namespace Metadata_Editor
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            using (WordprocessingDocument doc = WordprocessingDocument.Open(filepath, true))
+            {
+                var packageProps = doc.PackageProperties;
+
+                if (packageProps != null)
+                {
+                    // Генерируем случайные временные метки
+                    var random = new Random();
+                    DateTime now = DateTime.Now;
+
+                    // @ERROR НЕ РАБОТАЕТ ВАЩЕ
+                    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    // Случайное время редактирования (9-25 минут)
+                    // @ERROR НЕ РАБОТАЕТ ВАЩЕ
+                    int editMinutes = random.Next(9, 26);
+                    int editSeconds = random.Next(0, 60);
+                    TimeSpan editTime = new TimeSpan(0, editMinutes, editSeconds);
+
+                    // Дата создания - случайное время в прошлом (до 30 дней)
+                    DateTime created = now.AddDays(-random.Next(1, 31))
+                                       .AddHours(-random.Next(0, 24))
+                                       .AddMinutes(-random.Next(0, 60));
+
+                    // Дата изменения = дата создания + время редактирования
+                    DateTime modified = created.Add(editTime);
+
+                    // Дата последнего открытия - между созданием и текущим временем
+                    DateTime lastPrinted = created.AddDays(random.Next(0, (now - created).Days));
+
+                    // Устанавливаем свойства
+                    packageProps.Creator = userName; // Удаляем автора
+                    packageProps.LastModifiedBy = userName; // Удаляем "Кем сохранен"
+                    packageProps.Created = created;
+                    packageProps.Modified = modified;
+                    packageProps.LastPrinted = lastPrinted;
+
+
+                    // Дополнительные свойства для правдоподобности
+                    packageProps.Revision = "1";
+                    packageProps.Version = "1";
+                    doc.Save();
+                }
+            }
         }
     }
 }
